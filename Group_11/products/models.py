168d295f040448c2172b0ab5a0e13ae87cafa899
orlando-yerter <teczervice@gmail.com>
2022-10-31 22:47:10 -0300

    creando clases del models en productos

--- Group_11/products/models.py
+++ Group_11/products/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model):
+    nombre=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    tlfono=models.CharField(max_length=10)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+     
+    
+    class Meta:
+        verbose_name="cliente"
+        verbose_name_plural="clientes"
+
+    def __str__(self):
+        return self.nombre
+  
+
+
+class CategoriaProd(models.Model):
+    nombre=models.CharField(max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name="categoriaProd"
+        verbose_name_plural="categoriasProd"
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre=models.CharField(max_length=50)
+    categorias=models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
+    imagen=models.ImageField(upload_to="tienda", null=True, blank=True)
+    precio=models.FloatField()
+    disponibilidad=models.BooleanField(default=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+
+  
+
+    class Meta:
+        verbose_name="Producto"
+        verbose_name_plural="Productos"
